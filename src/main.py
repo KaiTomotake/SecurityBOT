edae69f9ede594a958ff82fd2ee11bef24abc7b8
Shiumano <takkunmail2005@gmail.com>
2026-01-06 07:15:57 +0000

    log_styleのテキストを逆にする

--- src/main.py
+++ src/main.py
@@ -23,9 +23,9 @@ def create_log(title: str, log_style: LogStyle, executor: discord.Member) -> dis
     FOOTER_HEAD = "SecurityBOT | "
     match log_style:
         case LogStyle.Success:
-            embed.set_footer(text="{}ErrorReport".format(FOOTER_HEAD))
-        case LogStyle.Error:
             embed.set_footer(text="{}ActionLog".format(FOOTER_HEAD))
+        case LogStyle.Error:
+            embed.set_footer(text="{}ErrorReport".format(FOOTER_HEAD))
     embed.add_field(
         name="実行者", value="{} (`{}`)".format(executor.name, executor.id), inline=False
     )
